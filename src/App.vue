ef86021a64771cad4e9b5e16dc5b1d8c1aab4ddd
bud_qi <11490663+bud-qi@user.noreply.gitee.com>
2022-09-06 18:12:28 +0800

    feat:登录页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    黑马
-    <!-- yarn add vant@latest-v2 -S -->
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box">nihao</div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -18,14 +11,11 @@
 //  - yarn add amfe-flexible
 //  2.盒子的使用
 //  - 自动的识别css中px，并将其转成rem
-export default {}
+export default {
+  data() {
+    return {}
+  }
+}
 </script>
 
-<style scoped>
-.box {
-  width: 689px;
-  height: 88px;
-  font-size: 25px;
-  background-color: red;
-}
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
+//  SPA
+//  首屏加载速度很慢
+//  路由懒加载
+//  - 使用到了页面，再去请求他
 const routes = [
-
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
